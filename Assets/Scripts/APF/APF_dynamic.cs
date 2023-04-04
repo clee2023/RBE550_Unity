6359de53520d6ae789c843a7f29e765d90ad7a96
nicklesscs <tgao2@wpi.edu>
2023-04-03 20:48:20 -0400

    update

--- Assets/Scripts/APF/APF_dynamic.cs
+++ Assets/Scripts/APF/APF_dynamic.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class APF_dyamic : MonoBehaviour
+public class APF_dynamic : MonoBehaviour
 {
     // The goal object to move towards
     public Transform goal;
@@ -65,39 +65,5 @@ public class APF_dyamic : MonoBehaviour
         // Calculate repulsion force from dynamic obstacles
         foreach (Transform dynamicObstacle in dynamicObstacles)
         {
-            Vector3 dynamicObstacleDirection = transform.position - dynamicObstacle.position;
-            float distanceToDynamicObstacle = dynamicObstacleDirection.magnitude;
-
-            if (distanceToDynamicObstacle < repulsionRadius)
-            {
-                float normalizedDistance = distanceToDynamicObstacle / repulsionRadius;
-                float repulsionForceMagnitude = maxDynamicRepulsionForce * (1 - Mathf.Pow(normalizedDistance, 2));
-                repulsionForce += dynamicObstacleDirection.normalized * repulsionForceMagnitude;
-            }
-        }
-
-        // Add a force towards the goal, while also adding repulsion force from obstacles
-        Vector3 directionToGoal = goal.position - transform.position;
-        Vector3 totalForce = directionToGoal.normalized * speed + repulsionForce;
-        rb.AddForce(totalForce);
-
-        // Limit the speed of the object
-        rb.velocity = Vector3.ClampMagnitude(rb.velocity, speed);
-
-        // Rotate the object to face the direction of movement
-        if (rb.velocity != Vector3.zero)
-        {
-            transform.rotation = Quaternion.LookRotation(rb.velocity, transform.up);
-        }
-
-        // Stop moving if the object is close enough to the goal
-        if (Vector3.Distance(transform.position, goal.position) < minDistance)
-        {
-            rb.velocity = Vector3.zero;
-        }
-    }
-
-    void OnTriggerEnter(Collider other)
-    {
-        // Add dynamic obstacles to the
+            Vector3 dynamicObstacleDirection = transform.position - dynamicObstacle
 
