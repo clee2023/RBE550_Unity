b3916106e109586d758f2b3885c09493f40cc3c3
nicklesscs <tgao2@wpi.edu>
2023-04-12 12:03:46 -0400

    update

--- Assets/Scripts/APF/APF.cs
+++ Assets/Scripts/APF/APF.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+/*using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -75,5 +75,5 @@ public class APF : MonoBehaviour
 
         return attractionForce;
     }
-}
+}*/
 
